0579def54b4dcc9b52559f281b2d9232e5c6c5e1
jgonzalezn06 <102035182+jgonzalezn06@users.noreply.github.com>
2022-07-27 11:52:57 -0400

    Add files via upload

---
+++ auto-recuperacion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ESTE SCRIPT ENCUENTRA UNA PALABRA CLAVE EN ESPECIFICO "DE ERROR" Y REINICIA EL CONTENEDOR
+
+FILE=$(docker logs -n 100 container)
+SEARCH="Caused by: java.sql.SQLTransientConnectionException: HikariPool-1 - Connection is not available, request timed out after 30000ms."
+if echo $FILE | grep -q "$SEARCH" ; then
+    docker restart container
+    exit 1
+fi
