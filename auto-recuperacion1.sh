6bb4665b122672042fe65a4a505e0b1e0e5097fe
Jahaziel Gonzalez <102035182+jgonzalezn06@users.noreply.github.com>
2022-08-10 13:00:37 -0400

    Update auto-recuperacion1.sh

--- auto-recuperacion1.sh
+++ auto-recuperacion1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# ESTE SCRIPT ENCUENTRA UNA PALABRA CLAVE EN ESPECIFICO "DE ERROR" Y REINICIA EL CONTENEDOR
-
+# ESTE SCRIPT ENCUENTRA UNA PALABRA CLAVE EN ESPECIFICO "DE ERROR" Y REINICIA EL SERVICIO
 FILE=$(docker logs -n 100 container)
 SEARCH="Caused by: java.sql.SQLTransientConnectionException: HikariPool-1 - Connection is not available, request timed out after 30000ms."
 if echo $FILE | grep -q "$SEARCH" ; then
