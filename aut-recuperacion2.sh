ffccfaa8c4bb4d13d130d25309502ab40d12c8a6
Jahaziel Gonzalez <102035182+jgonzalezn06@users.noreply.github.com>
2022-08-10 12:59:31 -0400

    Update aut-recuperacion2.sh

--- aut-recuperacion2.sh
+++ aut-recuperacion2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# SI ENCUENTRA 4 O MAS ERRORES EN LAS ULTIMAS 500 LINEAS DEL FICHERO SE REINICIARA EL SERVICIO
 FILE=$(tail -500 /file | grep -i 'error' | wc -l)
 if [ $FILE -gt 4 ] ; then
     /usr/bin/comando restart
